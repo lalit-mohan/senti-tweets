d3727ccf4d552ba9fdb29905bff960edbdeaf3b7
lol.lit <ilalitmohan@outlook.com>
2018-06-23 01:49:18 +0530

    Add files via upload

---
+++ senti-tweets.py
@@ -0,0 +1,29 @@
+import csv
+import tweepy
+from textblob import TextBlob
+
+consumer_key = #
+c_key_sec = #
+
+access_token = #
+a_token_sec = #
+
+auth = tweepy.OAuthHandler(consumer_key,c_key_sec)
+auth.set_access_token(access_token,a_token_sec)
+
+api = tweepy.API(auth)
+search_string = str(input("Enter search string:"))
+
+pub_tweets =api.search(search_string)
+tweet_l = []
+tweet_analysis = []
+
+
+with open('tweets.csv', 'w') as csvfile1:
+	writetweet = csv.writer(csvfile1, delimiter = ' ')
+	for tweet in  pub_tweets:
+		writetweet.writerow([tweet.text])
+		analysis = TextBlob(tweet.text)
+		writetweet.writerow([analysis.sentiment])
+
+print("Ten tweets written to csv file")
